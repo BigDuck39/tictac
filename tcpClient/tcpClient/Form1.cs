9a8314d725acdb648c6d2b75ba2fe2202b9ade30
BigDuck39 <81506772+BigDuck39@users.noreply.github.com>
2022-04-26 10:54:09 +0200

    Add files via upload

---
+++ tcpClient/tcpClient/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tcpClient {
+    public partial class Form1 : Form {
+        Int32 port = 13000;
+        TcpClient client = new TcpClient("127.0.0.1", 13000);
+        public void Connect(String server, String message) {
+            try {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer
+                // connected to the same address as specified by the server, port
+                // combination.
+
+
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            } catch (ArgumentNullException e) {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            } catch (SocketException e) {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+        public Form1() {
+            InitializeComponent();
+        }
+        bool server_started = false;
+        private void button1_Click(object sender, EventArgs e) {
+            server_started = true;
+            Connect("127.0.0.1", textBox1.Text);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            if (server_started) {
+                client.Close();
+                server_started = false;
+            }
+        }
+    }
+}
---
+++ tcpServer/tcpServer/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tcpServer {
+    public partial class Form1 : Form {
+        TcpListener server = null;
+        public void startServer() {
+
+            try {
+                // Set the TcpListener on port 13000.
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+
+                // Start listening for client requests.
+                server.Start();
+
+                // Buffer for reading data
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                // Enter the listening loop.
+                while (server_started) {
+                    listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add("Waiting for a connection".ToString())));
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    // You could also use server.AcceptSocket() here.
+                    TcpClient client = server.AcceptTcpClient();
+                    listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add("Connected".ToString())));
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    // Get a stream object for reading and writing
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+
+                    // Loop to receive all the data sent by the client.
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0) {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add(("Recived: " +data).ToString())));
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add("Sent: " + data).ToString()));
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                    // Shutdown and end connection
+                    client.Close();
+                }
+            } catch (SocketException e) {
+                listBox1.Invoke((MethodInvoker)(() => listBox1.Items.Add(e.ToString())));
+                Console.WriteLine("SocketException: {0}", e);
+            } finally {
+                // Stop listening for new clients.
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+        public Form1() {
+            InitializeComponent();
+        }
+        Thread th1;
+        bool server_started = false;
+        private void button1_Click(object sender, EventArgs e) {
+            server_started = true;
+            
+            th1 = new Thread(startServer);
+
+            th1.Start();
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            if (server_started) {
+                server.Stop();
+                server_started = false;
+                th1.Join();
+            }
+        }
+    }
+}
